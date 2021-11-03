c4ce920d0dc7ecec8f7f574adcccf5d8acfaef54
Jimmie Haskell <44525646+jimmiehaskell@users.noreply.github.com>
2021-11-03 10:24:21 -0300

    Update config.php

--- config/config.php
+++ config/config.php
@@ -3,6 +3,6 @@
     // Mysql
     const DBDRIVE = 'mysql';
     const DBHOST = 'localhost';
-    const DBNAME = 'php-rest-api';
-    const DBUSER = 'dev-app';
+    const DBNAME = '';
+    const DBUSER = '';
     const DBPASS = '';
